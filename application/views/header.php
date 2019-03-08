8f41e238322958cfa483fbfe510399ef29a5df86
Luis Garcia <luisgarcia.ing@gmail.com>
2019-03-07 19:07:38 -0800

    Updates that hadn't been committed

--- application/views/header.php
+++ application/views/header.php
@@ -45,7 +45,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="<?php echo base_url(); ?>">PATA</a>
+                <a class="navbar-brand" href="<?php echo base_url(); ?>"><!-- <img src="<?php echo base_url("img/logonew.png"); ?>" alt=""> -->PATA</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
